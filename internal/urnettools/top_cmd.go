full-bars/urnetwork-3.23-fix

feat(urnet-tools): top, a live full-screen view of a provider
Reads only the control socket snapshot: throughput graph for the last 10
minutes, current and average rate, clients, proxy pool, memory and
descriptors, and recent events. Also reachable as urtop. Keys: q/Esc/Ctrl-C
quit, Tab switches provider, +/- change the refresh rate, ? help. When the
provider stops answering it stays up, shows DISCONNECTED with a countdown, and
resumes by itself. Refuses without an interactive terminal, and gives the
terminal back on every exit path including a panic.

internal/urnettools/top_cmd.go
@@ -1,0 +1,114 @@
+package urnettools
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+
+	"golang.org/x/term"
+
+	"github.com/urnetwork/connect/internal/tui"
+	"github.com/urnetwork/connect/internal/tui/tcellui"
+)
+
+// Seams so the command is testable without a terminal or live providers.
+var (
+	topStdoutIsTerminal = func() bool { return term.IsTerminal(int(os.Stdout.Fd())) }
+	topOpenScreen       = tcellui.Open
+	topSourceFn         = func() topSource { return socketTopSource{} }
+)
+
+// cmdTop implements `urnet-tools top [target] [--interval D]`, the live
+// full-screen view. Target selection is status's: the same flags, the same
+// auto-narrowing to the one provider an unprivileged caller can reach, and the
+// same cross-user re-exec under sudo.
+func cmdTop(args []string) error {
+	interval, targetArgs, err := parseTopFlags(args)
+	if err != nil {
+		return err
+	}
+	// Checked first: with no terminal there is nothing to draw on, and the
+	// question of which provider to show has no bearing on that.
+	if !topStdoutIsTerminal() {
+		return errors.New("top needs an interactive terminal (stdout is not one); use `urnet-tools status` for one-shot or scripted output, add --json for machine-readable output")
+	}
+	t, _, err := parseTargetFlags(targetArgs)
+	if err != nil {
+		return err
+	}
+	providers := discoverStatusFn()
+	p, _, err := selectTargetOrSoleAccessible(providers, t, false)
+	if err != nil {
+		// Several providers and no target: status summarizes them, top starts
+		// on the first and lets Tab move between them.
+		if hasExplicitTarget(t) || len(providers) < 2 {
+			return err
+		}
+		p = providers[0]
+	}
+	if elevated, err := maybeElevateForCrossUser("top", p, args, false, false); elevated {
+		return err
+	}
+	cur := 0
+	for i, q := range providers {
+		if matchKey(q) == matchKey(p) {
+			cur = i
+			break
+		}
+	}
+
+	scr, err := topOpenScreen()
+	if err != nil {
+		return fmt.Errorf("cannot open the terminal for top: %w (use `urnet-tools status` instead)", err)
+	}
+	// SIGINT and SIGTERM end the loop through the context, so the terminal is
+	// restored by runTop's own exit path rather than by the signal killing the
+	// process mid-frame. In raw mode Ctrl-C arrives as a key, not a signal, and
+	// is handled by the model.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	m := newTopModel(providers, cur, interval, tui.ThemeFromEnv(os.Getenv), time.Now)
+	return runTop(ctx, scr, m, topSourceFn(), topTick)
+}
+
+// parseTopFlags pulls --interval out of args, leaving the target flags for the
+// strict target parser. It accepts a Go duration (500ms, 2s) or plain seconds.
+func parseTopFlags(args []string) (interval time.Duration, rest []string, err error) {
+	interval = topDefaultInterval
+	for i := 0; i < len(args); i++ {
+		a := args[i]
+		var val string
+		switch {
+		case a == "--interval":
+			if i+1 >= len(args) {
+				return 0, nil, errors.New("--interval needs a value, for example --interval 500ms")
+			}
+			i++
+			val = args[i]
+		case strings.HasPrefix(a, "--interval="):
+			val = strings.TrimPrefix(a, "--interval=")
+		default:
+			rest = append(rest, a)
+			continue
+		}
+		d, perr := time.ParseDuration(val)
+		if perr != nil {
+			secs, ferr := strconv.ParseFloat(val, 64)
+			if ferr != nil {
+				return 0, nil, fmt.Errorf("--interval %q is not a duration (try 500ms or 2s)", val)
+			}
+			d = time.Duration(secs * float64(time.Second))
+		}
+		if d < topMinInterval {
+			return 0, nil, fmt.Errorf("--interval must be at least %s", topMinInterval)
+		}
+		interval = d
+	}
+	return interval, rest, nil
+}

internal/urnettools/top_cmd_test.go
@@ -1,0 +1,76 @@
+package urnettools
+
+import (
+	"errors"
+	"strings"
+	"testing"
+
+	"github.com/urnetwork/connect/internal/tui/tcellui"
+)
+
+// stubTopCmd swaps the terminal and screen seams and isolates HOME, returning
+// counters for how often discovery ran and a screen was opened.
+func stubTopCmd(t *testing.T, isTTY bool, providers []Provider, openErr error) (discovered, opened *int) {
+	t.Helper()
+	stubStatus(t, providers, nil, false)
+	origTTY, origOpen := topStdoutIsTerminal, topOpenScreen
+	t.Cleanup(func() { topStdoutIsTerminal, topOpenScreen = origTTY, origOpen })
+	discovered, opened = new(int), new(int)
+	origDisc := discoverStatusFn
+	discoverStatusFn = func() []Provider { *discovered++; return origDisc() }
+	topStdoutIsTerminal = func() bool { return isTTY }
+	topOpenScreen = func() (tcellui.Screen, error) {
+		*opened++
+		return nil, openErr
+	}
+	return discovered, opened
+}
+
+// `urtop` from cron, a pipe or a redirect has no terminal to draw on. It must
+// say so and point at the scriptable command, and it must do so before it
+// discovers providers or touches the terminal.
+func TestCmdTopRefusesWithoutATerminalBeforeDoingAnythingElse(t *testing.T) {
+	discovered, opened := stubTopCmd(t, false, []Provider{{Unit: "a", Running: true}}, nil)
+
+	err := cmdTop(nil)
+	if err == nil {
+		t.Fatal("top without a terminal must fail")
+	}
+	for _, want := range []string{"interactive terminal", "urnet-tools status", "--json"} {
+		if !strings.Contains(err.Error(), want) {
+			t.Fatalf("error %q should mention %q", err, want)
+		}
+	}
+	if *discovered != 0 || *opened != 0 {
+		t.Fatalf("nothing else may run first: discovery=%d screenOpens=%d", *discovered, *opened)
+	}
+}
+
+// A bad flag is the operator's mistake and is reported as such, even without a
+// terminal, since it would fail the same way on one.
+func TestCmdTopRejectsABadIntervalBeforeTheTerminalCheck(t *testing.T) {
+	stubTopCmd(t, false, nil, nil)
+	err := cmdTop([]string{"--interval", "10ms"})
+	if err == nil || !strings.Contains(err.Error(), "at least") {
+		t.Fatalf("a too-small --interval should be rejected, got %v", err)
+	}
+}
+
+// If the terminal cannot be opened (a dumb terminal, a broken TERM), the error
+// names the alternative rather than dumping a tcell error alone.
+func TestCmdTopReportsAScreenThatCannotOpen(t *testing.T) {
+	_, opened := stubTopCmd(t, true, []Provider{{Unit: "a", Running: true}}, errors.New("terminal too dumb"))
+
+	err := cmdTop(nil)
+	if err == nil {
+		t.Fatal("an unopenable screen must fail")
+	}
+	if *opened != 1 {
+		t.Fatalf("the screen should be tried once, got %d", *opened)
+	}
+	for _, want := range []string{"cannot open the terminal", "terminal too dumb", "urnet-tools status"} {
+		if !strings.Contains(err.Error(), want) {
+			t.Fatalf("error %q should mention %q", err, want)
+		}
+	}
+}

internal/urnettools/top_run.go
@@ -1,0 +1,88 @@
+package urnettools
+
+import (
+	"context"
+	"fmt"
+	"runtime/debug"
+	"time"
+
+	"github.com/urnetwork/connect/internal/tui"
+	"github.com/urnetwork/connect/internal/tui/tcellui"
+)
+
+// topTick is how often the loop wakes to poll and to redraw the clock and the
+// DISCONNECTED countdown. It is the poll floor, so any interval that is a
+// multiple of it is met exactly.
+const topTick = topMinInterval
+
+// topResult is one finished fetch, tagged with the selection it was for.
+type topResult struct {
+	gen  int
+	snap *NodeSnapshot
+	err  error
+}
+
+// runTop drives the model on a screen until the user quits, ctx is cancelled
+// (SIGINT and SIGTERM cancel it), or the screen goes away. The terminal is
+// given back on every way out, including a panic: a terminal left in raw mode
+// on the alternate screen is the classic failure of a full-screen tool, so the
+// restore does not depend on the happy path.
+func runTop(ctx context.Context, scr tcellui.Screen, m *topModel, src topSource, tick time.Duration) (err error) {
+	defer func() {
+		r := recover()
+		scr.Close()
+		if r != nil {
+			err = fmt.Errorf("urnet-tools top crashed: %v\n%s", r, debug.Stack())
+		}
+	}()
+
+	results := make(chan topResult, 8)
+	fetch := func(gen int, p Provider) {
+		go func() {
+			var res topResult
+			res.gen = gen
+			// A panic in a fetch goroutine would kill the process with the
+			// terminal still in raw mode, so it is turned into an error the
+			// model shows as a lost provider.
+			defer func() {
+				if r := recover(); r != nil {
+					res.snap, res.err = nil, fmt.Errorf("snapshot fetch panicked: %v", r)
+				}
+				select {
+				case results <- res:
+				case <-ctx.Done():
+				}
+			}()
+			res.snap, res.err = src.Fetch(p)
+		}()
+	}
+
+	ticker := time.NewTicker(tick)
+	defer ticker.Stop()
+	buf := tui.New(0, 0)
+	for {
+		if gen, p, ok := m.wantFetch(m.now()); ok {
+			fetch(gen, p)
+		}
+		w, h := scr.Size()
+		buf.Resize(w, h)
+		buf.Fill(buf.Rect(), ' ', tui.Style{})
+		m.render(buf)
+		scr.Draw(buf)
+
+		select {
+		case <-ctx.Done():
+			return nil
+		case ev, ok := <-scr.Events():
+			if !ok {
+				return nil
+			}
+			if m.handle(ev) == topQuit {
+				return nil
+			}
+		case r := <-results:
+			m.apply(r.gen, r.snap, r.err)
+		case <-ticker.C:
+		}
+	}
+}

full-bars/sn

internal/urnettools/top_cmd.go
@@ -1,0 +1,114 @@
+package urnettools
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+
+	"golang.org/x/term"
+
+	"github.com/urfoundation/sn/internal/tui"
+	"github.com/urfoundation/sn/internal/tui/tcellui"
+)
+
+// Seams so the command is testable without a terminal or live providers.
+var (
+	topStdoutIsTerminal = func() bool { return term.IsTerminal(int(os.Stdout.Fd())) }
+	topOpenScreen       = tcellui.Open
+	topSourceFn         = func() topSource { return socketTopSource{} }
+)
+
+// cmdTop implements `urnet-tools top [target] [--interval D]`, the live
+// full-screen view. Target selection is status's: the same flags, the same
+// auto-narrowing to the one provider an unprivileged caller can reach, and the
+// same cross-user re-exec under sudo.
+func cmdTop(args []string) error {
+	interval, targetArgs, err := parseTopFlags(args)
+	if err != nil {
+		return err
+	}
+	// Checked first: with no terminal there is nothing to draw on, and the
+	// question of which provider to show has no bearing on that.
+	if !topStdoutIsTerminal() {
+		return errors.New("top needs an interactive terminal (stdout is not one); use `urnet-tools status` for one-shot or scripted output, add --json for machine-readable output")
+	}
+	t, _, err := parseTargetFlags(targetArgs)
+	if err != nil {
+		return err
+	}
+	providers := discoverStatusFn()
+	p, _, err := selectTargetOrSoleAccessible(providers, t, false)
+	if err != nil {
+		// Several providers and no target: status summarizes them, top starts
+		// on the first and lets Tab move between them.
+		if hasExplicitTarget(t) || len(providers) < 2 {
+			return err
+		}
+		p = providers[0]
+	}
+	if elevated, err := maybeElevateForCrossUser("top", p, args, false, false); elevated {
+		return err
+	}
+	cur := 0
+	for i, q := range providers {
+		if matchKey(q) == matchKey(p) {
+			cur = i
+			break
+		}
+	}
+
+	scr, err := topOpenScreen()
+	if err != nil {
+		return fmt.Errorf("cannot open the terminal for top: %w (use `urnet-tools status` instead)", err)
+	}
+	// SIGINT and SIGTERM end the loop through the context, so the terminal is
+	// restored by runTop's own exit path rather than by the signal killing the
+	// process mid-frame. In raw mode Ctrl-C arrives as a key, not a signal, and
+	// is handled by the model.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	m := newTopModel(providers, cur, interval, tui.ThemeFromEnv(os.Getenv), time.Now)
+	return runTop(ctx, scr, m, topSourceFn(), topTick)
+}
+
+// parseTopFlags pulls --interval out of args, leaving the target flags for the
+// strict target parser. It accepts a Go duration (500ms, 2s) or plain seconds.
+func parseTopFlags(args []string) (interval time.Duration, rest []string, err error) {
+	interval = topDefaultInterval
+	for i := 0; i < len(args); i++ {
+		a := args[i]
+		var val string
+		switch {
+		case a == "--interval":
+			if i+1 >= len(args) {
+				return 0, nil, errors.New("--interval needs a value, for example --interval 500ms")
+			}
+			i++
+			val = args[i]
+		case strings.HasPrefix(a, "--interval="):
+			val = strings.TrimPrefix(a, "--interval=")
+		default:
+			rest = append(rest, a)
+			continue
+		}
+		d, perr := time.ParseDuration(val)
+		if perr != nil {
+			secs, ferr := strconv.ParseFloat(val, 64)
+			if ferr != nil {
+				return 0, nil, fmt.Errorf("--interval %q is not a duration (try 500ms or 2s)", val)
+			}
+			d = time.Duration(secs * float64(time.Second))
+		}
+		if d < topMinInterval {
+			return 0, nil, fmt.Errorf("--interval must be at least %s", topMinInterval)
+		}
+		interval = d
+	}
+	return interval, rest, nil
+}

internal/urnettools/top_cmd_test.go
@@ -1,0 +1,76 @@
+package urnettools
+
+import (
+	"errors"
+	"strings"
+	"testing"
+
+	"github.com/urfoundation/sn/internal/tui/tcellui"
+)
+
+// stubTopCmd swaps the terminal and screen seams and isolates HOME, returning
+// counters for how often discovery ran and a screen was opened.
+func stubTopCmd(t *testing.T, isTTY bool, providers []Provider, openErr error) (discovered, opened *int) {
+	t.Helper()
+	stubStatus(t, providers, nil, false)
+	origTTY, origOpen := topStdoutIsTerminal, topOpenScreen
+	t.Cleanup(func() { topStdoutIsTerminal, topOpenScreen = origTTY, origOpen })
+	discovered, opened = new(int), new(int)
+	origDisc := discoverStatusFn
+	discoverStatusFn = func() []Provider { *discovered++; return origDisc() }
+	topStdoutIsTerminal = func() bool { return isTTY }
+	topOpenScreen = func() (tcellui.Screen, error) {
+		*opened++
+		return nil, openErr
+	}
+	return discovered, opened
+}
+
+// `urtop` from cron, a pipe or a redirect has no terminal to draw on. It must
+// say so and point at the scriptable command, and it must do so before it
+// discovers providers or touches the terminal.
+func TestCmdTopRefusesWithoutATerminalBeforeDoingAnythingElse(t *testing.T) {
+	discovered, opened := stubTopCmd(t, false, []Provider{{Unit: "a", Running: true}}, nil)
+
+	err := cmdTop(nil)
+	if err == nil {
+		t.Fatal("top without a terminal must fail")
+	}
+	for _, want := range []string{"interactive terminal", "urnet-tools status", "--json"} {
+		if !strings.Contains(err.Error(), want) {
+			t.Fatalf("error %q should mention %q", err, want)
+		}
+	}
+	if *discovered != 0 || *opened != 0 {
+		t.Fatalf("nothing else may run first: discovery=%d screenOpens=%d", *discovered, *opened)
+	}
+}
+
+// A bad flag is the operator's mistake and is reported as such, even without a
+// terminal, since it would fail the same way on one.
+func TestCmdTopRejectsABadIntervalBeforeTheTerminalCheck(t *testing.T) {
+	stubTopCmd(t, false, nil, nil)
+	err := cmdTop([]string{"--interval", "10ms"})
+	if err == nil || !strings.Contains(err.Error(), "at least") {
+		t.Fatalf("a too-small --interval should be rejected, got %v", err)
+	}
+}
+
+// If the terminal cannot be opened (a dumb terminal, a broken TERM), the error
+// names the alternative rather than dumping a tcell error alone.
+func TestCmdTopReportsAScreenThatCannotOpen(t *testing.T) {
+	_, opened := stubTopCmd(t, true, []Provider{{Unit: "a", Running: true}}, errors.New("terminal too dumb"))
+
+	err := cmdTop(nil)
+	if err == nil {
+		t.Fatal("an unopenable screen must fail")
+	}
+	if *opened != 1 {
+		t.Fatalf("the screen should be tried once, got %d", *opened)
+	}
+	for _, want := range []string{"cannot open the terminal", "terminal too dumb", "urnet-tools status"} {
+		if !strings.Contains(err.Error(), want) {
+			t.Fatalf("error %q should mention %q", err, want)
+		}
+	}
+}

internal/urnettools/top_run.go
@@ -1,0 +1,88 @@
+package urnettools
+
+import (
+	"context"
+	"fmt"
+	"runtime/debug"
+	"time"
+
+	"github.com/urfoundation/sn/internal/tui"
+	"github.com/urfoundation/sn/internal/tui/tcellui"
+)
+
+// topTick is how often the loop wakes to poll and to redraw the clock and the
+// DISCONNECTED countdown. It is the poll floor, so any interval that is a
+// multiple of it is met exactly.
+const topTick = topMinInterval
+
+// topResult is one finished fetch, tagged with the selection it was for.
+type topResult struct {
+	gen  int
+	snap *NodeSnapshot
+	err  error
+}
+
+// runTop drives the model on a screen until the user quits, ctx is cancelled
+// (SIGINT and SIGTERM cancel it), or the screen goes away. The terminal is
+// given back on every way out, including a panic: a terminal left in raw mode
+// on the alternate screen is the classic failure of a full-screen tool, so the
+// restore does not depend on the happy path.
+func runTop(ctx context.Context, scr tcellui.Screen, m *topModel, src topSource, tick time.Duration) (err error) {
+	defer func() {
+		r := recover()
+		scr.Close()
+		if r != nil {
+			err = fmt.Errorf("urnet-tools top crashed: %v\n%s", r, debug.Stack())
+		}
+	}()
+
+	results := make(chan topResult, 8)
+	fetch := func(gen int, p Provider) {
+		go func() {
+			var res topResult
+			res.gen = gen
+			// A panic in a fetch goroutine would kill the process with the
+			// terminal still in raw mode, so it is turned into an error the
+			// model shows as a lost provider.
+			defer func() {
+				if r := recover(); r != nil {
+					res.snap, res.err = nil, fmt.Errorf("snapshot fetch panicked: %v", r)
+				}
+				select {
+				case results <- res:
+				case <-ctx.Done():
+				}
+			}()
+			res.snap, res.err = src.Fetch(p)
+		}()
+	}
+
+	ticker := time.NewTicker(tick)
+	defer ticker.Stop()
+	buf := tui.New(0, 0)
+	for {
+		if gen, p, ok := m.wantFetch(m.now()); ok {
+			fetch(gen, p)
+		}
+		w, h := scr.Size()
+		buf.Resize(w, h)
+		buf.Fill(buf.Rect(), ' ', tui.Style{})
+		m.render(buf)
+		scr.Draw(buf)
+
+		select {
+		case <-ctx.Done():
+			return nil
+		case ev, ok := <-scr.Events():
+			if !ok {
+				return nil
+			}
+			if m.handle(ev) == topQuit {
+				return nil
+			}
+		case r := <-results:
+			m.apply(r.gen, r.snap, r.err)
+		case <-ticker.C:
+		}
+	}
+}